3c9c6d91f0c3c041af4def5686982a849ec53522
Ronualdo Maciel <user.email>
2016-03-17 17:15:07 -0300

    Creates getMockedRequestWithParameters method on test
    Creates method to help on the mock of HttpServletRequests

--- src/test/java/com/twilio/automatedsurvey/servlets/QuestionServletTest.java
+++ src/test/java/com/twilio/automatedsurvey/servlets/QuestionServletTest.java
@@ -3,7 +3,6 @@ package com.twilio.automatedsurvey.servlets;
 import com.twilio.automatedsurvey.survey.Question;
 import com.twilio.automatedsurvey.survey.Survey;
 import com.twilio.automatedsurvey.survey.SurveyRepository;
-import com.twilio.sdk.verbs.Say;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.TwiMLResponse;
 import org.junit.Test;
@@ -13,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -23,17 +24,30 @@ public class QuestionServletTest {
     public void shouldRespondWhenSurveyHasOnlyOneQuestion() throws TwiMLException, IOException {
         ResponseWriter responseWriter = mock(ResponseWriter.class);
         SurveyRepository surveyRepository = mock(SurveyRepository.class);
+
         Question voiceQuestion = new Question("Is that a question?", "voice");
         when(surveyRepository.find(anyLong())).thenReturn(Optional.of(surveyWithQuestion(voiceQuestion)));
+
         HttpServletResponse servletResponse = mock(HttpServletResponse.class);
-        HttpServletRequest servletRequest = mock(HttpServletRequest.class);
-        when(servletRequest.getParameter(anyString())).thenReturn("1");
+        HttpServletRequest servletRequest = getMockedRequestWithParameters(new HashMap(){{
+            put("question", "1");
+            put("survey", "1");
+        }});
 
         QuestionServlet questionServlet = new QuestionServlet(surveyRepository, responseWriter);
 
         questionServlet.doGet(servletRequest, servletResponse);
 
         verify(responseWriter, times(1)).writeIn(eq(servletResponse), any(TwiMLResponse.class));
+    }
+
+    private HttpServletRequest getMockedRequestWithParameters(Map<String, String> parameters) {
+        HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+
+        for (String key : parameters.keySet()) {
+            when(servletRequest.getParameter(key)).thenReturn(parameters.get(key));
+        }
+        return servletRequest;
     }
 
     private Survey surveyWithQuestion(Question... questions) {
